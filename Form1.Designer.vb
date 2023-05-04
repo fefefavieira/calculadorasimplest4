72e594eca6ec88a755d9c6b02fb9040bccfdc409
fefefavieira <132399750+fefefavieira@users.noreply.github.com>
2023-05-04 14:09:12 -0300

    calculadora
    calculadora simples

---
+++ Form1.Designer.vb
@@ -0,0 +1,356 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TxtDisplay = New System.Windows.Forms.TextBox()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.btnzero = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Btnquatro = New System.Windows.Forms.Button()
+        Me.Btnmenoss = New System.Windows.Forms.Button()
+        Me.Btnnove = New System.Windows.Forms.Button()
+        Me.Btnoito = New System.Windows.Forms.Button()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.Btncopy = New System.Windows.Forms.Button()
+        Me.Btnreset = New System.Windows.Forms.Button()
+        Me.Btnmenos = New System.Windows.Forms.Button()
+        Me.Btn = New System.Windows.Forms.Button()
+        Me.btncinco = New System.Windows.Forms.Button()
+        Me.Btnseis = New System.Windows.Forms.Button()
+        Me.Btnmulti = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Btndois = New System.Windows.Forms.Button()
+        Me.Bnttres = New System.Windows.Forms.Button()
+        Me.btnbarra = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.TxtDisplay, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 1)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 6)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(406, 441)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'TxtDisplay
+        '
+        Me.TxtDisplay.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TxtDisplay.Location = New System.Drawing.Point(3, 3)
+        Me.TxtDisplay.Multiline = True
+        Me.TxtDisplay.Name = "TxtDisplay"
+        Me.TxtDisplay.Size = New System.Drawing.Size(400, 82)
+        Me.TxtDisplay.TabIndex = 0
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 4
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.btnzero, 0, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.Button14, 0, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnigual, 0, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.Button10, 0, 4)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnquatro, 0, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnmenoss, 3, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnnove, 2, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnoito, 1, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnclear, 0, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btncopy, 1, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnreset, 2, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnmenos, 3, 0)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btn, 0, 1)
+        Me.TableLayoutPanel2.Controls.Add(Me.btncinco, 1, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnseis, 2, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btnmulti, 3, 2)
+        Me.TableLayoutPanel2.Controls.Add(Me.Button17, 0, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.Btndois, 1, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.Bnttres, 2, 3)
+        Me.TableLayoutPanel2.Controls.Add(Me.btnbarra, 3, 3)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 91)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 5
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.83333!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.83333!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.83333!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.83333!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(400, 347)
+        Me.TableLayoutPanel2.TabIndex = 1
+        '
+        'btnzero
+        '
+        Me.btnzero.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnzero.Location = New System.Drawing.Point(103, 291)
+        Me.btnzero.Name = "btnzero"
+        Me.btnzero.Size = New System.Drawing.Size(94, 53)
+        Me.btnzero.TabIndex = 14
+        Me.btnzero.Text = "0"
+        Me.btnzero.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button14.Location = New System.Drawing.Point(3, 291)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(94, 53)
+        Me.Button14.TabIndex = 13
+        Me.Button14.Text = "Button14"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'btnigual
+        '
+        Me.btnigual.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnigual.Location = New System.Drawing.Point(303, 291)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(94, 53)
+        Me.btnigual.TabIndex = 12
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button10.Location = New System.Drawing.Point(203, 291)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(94, 53)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "Button10"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Btnquatro
+        '
+        Me.Btnquatro.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnquatro.Location = New System.Drawing.Point(3, 147)
+        Me.Btnquatro.Name = "Btnquatro"
+        Me.Btnquatro.Size = New System.Drawing.Size(94, 66)
+        Me.Btnquatro.TabIndex = 8
+        Me.Btnquatro.Text = "4"
+        Me.Btnquatro.UseVisualStyleBackColor = True
+        '
+        'Btnmenoss
+        '
+        Me.Btnmenoss.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnmenoss.Location = New System.Drawing.Point(303, 75)
+        Me.Btnmenoss.Name = "Btnmenoss"
+        Me.Btnmenoss.Size = New System.Drawing.Size(94, 66)
+        Me.Btnmenoss.TabIndex = 7
+        Me.Btnmenoss.Text = "-"
+        Me.Btnmenoss.UseVisualStyleBackColor = True
+        '
+        'Btnnove
+        '
+        Me.Btnnove.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnnove.Location = New System.Drawing.Point(203, 75)
+        Me.Btnnove.Name = "Btnnove"
+        Me.Btnnove.Size = New System.Drawing.Size(94, 66)
+        Me.Btnnove.TabIndex = 6
+        Me.Btnnove.Text = "7"
+        Me.Btnnove.UseVisualStyleBackColor = True
+        '
+        'Btnoito
+        '
+        Me.Btnoito.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnoito.Location = New System.Drawing.Point(103, 75)
+        Me.Btnoito.Name = "Btnoito"
+        Me.Btnoito.Size = New System.Drawing.Size(94, 66)
+        Me.Btnoito.TabIndex = 5
+        Me.Btnoito.Text = "8"
+        Me.Btnoito.UseVisualStyleBackColor = True
+        '
+        'btnclear
+        '
+        Me.btnclear.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnclear.Location = New System.Drawing.Point(3, 3)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(94, 66)
+        Me.btnclear.TabIndex = 0
+        Me.btnclear.Text = "apagar"
+        Me.btnclear.UseVisualStyleBackColor = True
+        '
+        'Btncopy
+        '
+        Me.Btncopy.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btncopy.Location = New System.Drawing.Point(103, 3)
+        Me.Btncopy.Name = "Btncopy"
+        Me.Btncopy.Size = New System.Drawing.Size(94, 66)
+        Me.Btncopy.TabIndex = 1
+        Me.Btncopy.Text = "copiar"
+        Me.Btncopy.UseVisualStyleBackColor = True
+        '
+        'Btnreset
+        '
+        Me.Btnreset.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnreset.Location = New System.Drawing.Point(203, 3)
+        Me.Btnreset.Name = "Btnreset"
+        Me.Btnreset.Size = New System.Drawing.Size(94, 66)
+        Me.Btnreset.TabIndex = 2
+        Me.Btnreset.Text = "limpar"
+        Me.Btnreset.UseVisualStyleBackColor = True
+        '
+        'Btnmenos
+        '
+        Me.Btnmenos.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnmenos.Location = New System.Drawing.Point(303, 3)
+        Me.Btnmenos.Name = "Btnmenos"
+        Me.Btnmenos.Size = New System.Drawing.Size(94, 66)
+        Me.Btnmenos.TabIndex = 3
+        Me.Btnmenos.Text = "+"
+        Me.Btnmenos.UseVisualStyleBackColor = True
+        '
+        'Btn
+        '
+        Me.Btn.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btn.Location = New System.Drawing.Point(3, 75)
+        Me.Btn.Name = "Btn"
+        Me.Btn.Size = New System.Drawing.Size(94, 66)
+        Me.Btn.TabIndex = 4
+        Me.Btn.Text = "5"
+        Me.Btn.UseVisualStyleBackColor = True
+        '
+        'btncinco
+        '
+        Me.btncinco.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btncinco.Location = New System.Drawing.Point(103, 147)
+        Me.btncinco.Name = "btncinco"
+        Me.btncinco.Size = New System.Drawing.Size(94, 66)
+        Me.btncinco.TabIndex = 15
+        Me.btncinco.Text = "5"
+        Me.btncinco.UseVisualStyleBackColor = True
+        '
+        'Btnseis
+        '
+        Me.Btnseis.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnseis.Location = New System.Drawing.Point(203, 147)
+        Me.Btnseis.Name = "Btnseis"
+        Me.Btnseis.Size = New System.Drawing.Size(94, 66)
+        Me.Btnseis.TabIndex = 16
+        Me.Btnseis.Text = "6"
+        Me.Btnseis.UseVisualStyleBackColor = True
+        '
+        'Btnmulti
+        '
+        Me.Btnmulti.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btnmulti.Location = New System.Drawing.Point(303, 147)
+        Me.Btnmulti.Name = "Btnmulti"
+        Me.Btnmulti.Size = New System.Drawing.Size(94, 66)
+        Me.Btnmulti.TabIndex = 17
+        Me.Btnmulti.Text = "*"
+        Me.Btnmulti.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Button17.Location = New System.Drawing.Point(3, 219)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(94, 66)
+        Me.Button17.TabIndex = 18
+        Me.Button17.Text = "1"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Btndois
+        '
+        Me.Btndois.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Btndois.Location = New System.Drawing.Point(103, 219)
+        Me.Btndois.Name = "Btndois"
+        Me.Btndois.Size = New System.Drawing.Size(94, 66)
+        Me.Btndois.TabIndex = 19
+        Me.Btndois.Text = "2"
+        Me.Btndois.UseVisualStyleBackColor = True
+        '
+        'Bnttres
+        '
+        Me.Bnttres.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Bnttres.Location = New System.Drawing.Point(203, 219)
+        Me.Bnttres.Name = "Bnttres"
+        Me.Bnttres.Size = New System.Drawing.Size(94, 66)
+        Me.Bnttres.TabIndex = 20
+        Me.Bnttres.Text = "3"
+        Me.Bnttres.UseVisualStyleBackColor = True
+        '
+        'btnbarra
+        '
+        Me.btnbarra.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.btnbarra.Location = New System.Drawing.Point(303, 219)
+        Me.btnbarra.Name = "btnbarra"
+        Me.btnbarra.Size = New System.Drawing.Size(94, 66)
+        Me.btnbarra.TabIndex = 21
+        Me.btnbarra.Text = "/"
+        Me.btnbarra.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(407, 459)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "calculadora da fe"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TxtDisplay As TextBox
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents Btnmenoss As Button
+    Friend WithEvents Btnnove As Button
+    Friend WithEvents Btnoito As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents Btncopy As Button
+    Friend WithEvents Btnreset As Button
+    Friend WithEvents Btnmenos As Button
+    Friend WithEvents Btn As Button
+    Friend WithEvents Btnquatro As Button
+    Friend WithEvents btnzero As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents btnigual As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents btncinco As Button
+    Friend WithEvents Btnseis As Button
+    Friend WithEvents Btnmulti As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Btndois As Button
+    Friend WithEvents Bnttres As Button
+    Friend WithEvents btnbarra As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Btncopy_Click(sender As Object, e As EventArgs) Handles Btncopy.Click
+
+    End Sub
+End Class
